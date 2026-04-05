685ddea98a6c4dbcbac3efe7118e49bd655d5b21
Kattrule <kameler.katt@gmail.com>
2026-04-05 16:07:42 +0100

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,21 @@
+// En väldigt enkel Service Worker
+const CACHE_NAME = 'audio-recorder-v1';
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll([
+                './index.html',
+                './manifest.json'
+            ]);
+        })
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
+});
